0dbebcd06509b17f27ac3840ea9526a52d273ad3
ZIDAN44 <ZIDAN44@users.noreply.github.com>
2024-08-11 08:15:41 +0000

    spes: overlay: Convert overlays into RRO_Overlays
    Co-Authored-By: AndroidHQ254 <AndroidHQ254@users.noreply.github.com>
    Co-Authored-By: nivlafx <nivlafx@users.noreply.github.com>
    Co-Authored-By: joeyhuab <joeyhuab@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -384,12 +384,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_k7tn/com.android.nfc_extras.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
-# Overlays RRO
 PRODUCT_PACKAGES += \
     CarrierConfigSpes \
     DeviceAsWebcamSpes \
@@ -398,6 +392,9 @@ PRODUCT_PACKAGES += \
     SettingsSpes \
     SettingsProviderSpes \
     SystemUISpes \
+    LineageSDKSpes \
+    DailerSpes \
+    ApertureSpes \
     TelephonySpes \
     WifiResSpes
 
